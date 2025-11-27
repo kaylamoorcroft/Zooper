05b5a4b264032d904b2d58d936fac53e8cf0f327
kaylamoorcroft <kayla.moorcroft@icloud.com>
2025-11-26 22:28:26 -0400

    fix: remove unused function from Utils.java

--- Utils.java
+++ Utils.java
@@ -10,20 +10,4 @@ public abstract class Utils {
         }
         return result;
     }
-
-    //converts given int to matching location value
-    public static Location intToLocation(int target) {
-    	if(target == 1) {return Location.AFRICA;}
-		else if(target == 2) {return Location.ANTARCTICA;}
-		else if(target == 3) {return Location.ASIA;}
-		else if(target == 4) {return Location.AUSTRALIA;}
-		else if(target == 5) {return Location.EUROPE;}
-		else if(target == 6) {return Location.NORTH_AMERICA;}
-		else if(target == 7) {return Location.SOUTH_AMERICA;}
-		else if(target == 8) {return Location.UNKNOWN;}
-		else {
-			System.out.println("Invalid continent entered");
-			return Location.UNKNOWN;
-		}
-    }
 }
